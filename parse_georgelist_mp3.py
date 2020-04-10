0f24f998caa263a0cbf85424496154133ac5a470
Camilo Zamora <camilo.zamora@deltaits.com>
2020-04-09 21:01:39 -0500

    Ajustes a múltiples mp3 y manejo de errores

--- parse_georgelist_mp3.py
+++ parse_georgelist_mp3.py
@@ -20,11 +20,14 @@ def obtenerPistas(info_filepath):
     # if len(pistas)==1:
     #     pistas=[p.strip() for p in tracklist[:end_pistas+1].split("--")]
 
-    pistas_idx=[m.start() for m in re.finditer("[0-9]+\.",tracklist)]
+    pistas_idx=[m.start() for m in re.finditer("[0-9]+\.[^0-9]",tracklist)]
     pistas=[]
     for start,end in zip_longest(pistas_idx,pistas_idx[1:],fillvalue=-1):
         track=tracklist[start:end]
-        end_pista=track.rindex(")")
+        try:
+            end_pista=track.rindex(")")
+        except ValueError:
+            end_pista=end
         track=track[:end_pista+1]
         pistas.append(track)
     
@@ -33,23 +36,30 @@ def obtenerPistas(info_filepath):
 def parseTracks(trackinfo):
     tracknumber,_,track=trackinfo.partition(".")
 
-    name,_,length=track.rpartition("(")
-
-    mins,secs=length[:-1].split(":")
-    length_secs=int(secs)+60*int(mins)
+    try:
+        track=re.findall(".+\([0-9]+\:[0-9]+\)",track)[0] #busca hasta el tiempo
+        name,_,length=track.rpartition("(")    
+        mins,secs=length[:-1].split(":")
+        length_secs=int(secs)+60*int(mins)
+    except IndexError:
+        print(f"WARNING: Pista sin tiempos {track}")
+        name=track
+        length_secs=0   
+    
     return (tracknumber,name.strip(),length_secs)
 
-if __name__ == "__main__":
-    filename="Música interpretada por los Gaiteros de San Jacinto y preguntas - George List - 1965"
-    root="./output/George List"
+def ajustar(filename,root):
     info_filepath=f"{root}/{filename}/info.txt"
 
     #obtener nombres y largos de pistas
-    pistas=obtenerPistas(info_filepath)
+    try:
+        pistas=obtenerPistas(info_filepath)
+    except ValueError as e:
+        return f"ERROR: {str(e)}"
     tracklist=[parseTracks(t) for t in pistas]
 
-    audio_filepath=f"{root}/{filename}/{filename}.mp3"
-    audio=AudioSegment.from_mp3(audio_filepath)
+    audio_filepath=[o for o in os.scandir(f"{root}/{filename}") if "mp3" in o.name][0]
+    audio=AudioSegment.from_mp3(audio_filepath.path)
     audio = audio.set_frame_rate(48000)
 
     os.makedirs(f"{root}/{filename}/tracks", exist_ok=True)
@@ -59,6 +69,26 @@ if __name__ == "__main__":
         audio=audio[length:] #recorta la pista actual
 
         name=re.sub("[\\\/]","-",name)
-        name=re.sub("[\"\'\:]","",name)
+        name=re.sub("[\"\'\:\?]","",name)
+        name=name[:80]
         path_=f"{root}/{filename}/tracks/{name}.mp3"
-        track.export(path_, format="mp3",tags={"track":number})
+        try:
+            track.export(path_, format="mp3",tags={"track":number})
+        except Exception as e:
+            return f"ERROR: {str(e)}"
+
+    return f"Archivos exportados satisfactoriamente a {root}/{filename[:10]}.../tracks"
+
+if __name__ == "__main__":
+
+    root="./output/George List"
+
+    for obj in [o for o in os.scandir(root) if o.is_dir()]:
+        if "tracks" in os.listdir(obj):
+            # print(f"{obj.name[:10]}... ya ha sido ajustado previamente. Si desea hacerlo de nuevo, por favor borre la carpeta 'tracks'.")
+            continue
+        print(f"Ajustando {obj.name}...")
+        result=ajustar(obj.name,root)
+        print(result)
+    
+    
